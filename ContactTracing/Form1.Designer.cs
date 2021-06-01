98086c8071e10f5a808f05167b7ec68a9990e643
JoyAnnMatriano <ja.matriano24@gmail.com>
2021-06-01 23:27:17 +0800

    Add project files.

---
+++ ContactTracing/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ContactTracing
+{
+    partial class ContactTracing_form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enter_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enter_btn
+            // 
+            this.enter_btn.Location = new System.Drawing.Point(158, 219);
+            this.enter_btn.Name = "enter_btn";
+            this.enter_btn.Size = new System.Drawing.Size(147, 60);
+            this.enter_btn.TabIndex = 0;
+            this.enter_btn.Text = "Enter";
+            this.enter_btn.UseVisualStyleBackColor = true;
+            // 
+            // ContactTracing_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 668);
+            this.Controls.Add(this.enter_btn);
+            this.Name = "ContactTracing_form";
+            this.Text = "Contact Tracing Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enter_btn;
+    }
+}
+
---
+++ ContactTracing/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ContactTracing
+{
+    public partial class ContactTracing_form : Form
+    {
+        public ContactTracing_form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
