24fa8496bc8993d9c89da6f350e5f7a54e2ee19e
JoyAnnMatriano <ja.matriano24@gmail.com>
2021-07-10 03:18:57 +0800

    clean codes

--- ContactTracing/Form1.cs
+++ ContactTracing/Form1.cs
@@ -25,24 +25,24 @@ namespace ContactTracing
                 if (travel_options.SelectedItem == "Yes")
                 {
                     if (surName_bx.Text == "" || firstName_bx.Text == "" || age_bx.SelectedItem == null || sex_options.SelectedItem == null || temp_bx.Text == "" || add_bx.Text == "" || phone_bx.Text == ""
-                || travel_options.SelectedItem == null || place_trvl_bx.Text == "")
+                || travel_options.SelectedItem == null || place_trvl_bx.Text == "") //all NULL and placeTravelBox
                     {
                         MessageBox.Show("Please fill up all required fields!");
                     }
-                    else
+                    else //all data will proceed in the file text
                     {
                         ImportantInfo();
                         dataCompleted_message();
                     }
                 }
-                if (travel_options.SelectedItem == "No")
+                if (travel_options.SelectedItem == "No") //all NULL w/o placeTravelBox
                 {
                     if (surName_bx.Text == "" || firstName_bx.Text == "" || age_bx.SelectedItem == null || sex_options.SelectedItem == null || temp_bx.Text == "" || add_bx.Text == "" || phone_bx.Text == ""
                 || travel_options.SelectedItem == null)
                     {
                         MessageBox.Show("Please fill up all required fields!");
                     }
-                    else
+                    else //all data will proceed in the file text
                     {
                         ImportantInfo();
                         dataCompleted_message();
@@ -57,15 +57,16 @@ namespace ContactTracing
         }
         private void ContactTracing_form_Load(object sender, EventArgs e)
         {
+            //Added Options
             travel_options.Items.Clear();
             travel_options.Items.Add("Yes");
             travel_options.Items.Add("No");
-
+            //
             sex_options.Items.Clear();
             sex_options.Items.Add("Female");
             sex_options.Items.Add("Male");
             sex_options.Items.Add("prefer not to say");
-
+            //
             age_bx.Items.Clear();
             age_bx.Items.Add("15");
             age_bx.Items.Add("16");
@@ -121,7 +122,6 @@ namespace ContactTracing
         }
         private void travel_options_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             if (travel_options.SelectedItem == "Yes")
             {
                 place_trvl.Visible = true;
@@ -145,6 +145,7 @@ namespace ContactTracing
                                                      $"Did the customer travelled?: {travel_options.SelectedItem}",
                                                      $"(if YES) Place(s): {place_trvl_bx.Text}"));
         }
+        //================================PRIVATE VOIDS (made)
         private void dataCompleted_message()
         {
             string message = "Thank you for filling up " + firstName_bx.Text + "!";
